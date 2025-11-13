8c572cdea8286b9b3d3f40cb0d506cc75fab953a
Matheus Melo <162399194+matfels@users.noreply.github.com>
2025-11-13 15:27:52 -0300

    Inicio projeto React Native
    Fundamentos em React Native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importando os componentes View e Text do react-native
+import {View, Text} from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+// Exportando por padrão uma função vazia chamada App
+
+// --------------------------------- Regras para criar um componente -------------------------------- //
+// Componente tem que começar com letra maiúscula
+// O componente pode retornar um unico elemento pai, que pode ter varios elementos filhos dentro dele.
+export default function App(){ 
+
+  // Toda função precisa retornar algo, o conteudo retornado aqui será exibido na tela.
+  // View foi importado lá em cima, junto com o Text. "Essa View é o elemento pai" os elementos tem que estar dentro dela.
+
+  return(
+
+    <View style={{ flex:1, justifyContent:"center"}}>
+        <Text>Olá Mundo!</Text> 
+
     </View>
-  );
-}
+    
+    )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
