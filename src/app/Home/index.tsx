3a236c253269895298acc890b2af0ba4cceb664a
Matheus Melo <162399194+matfels@users.noreply.github.com>
2025-11-15 16:45:58 -0300

    Atualização da aula React
    Adicionamos imagens e criamos um Alias para o caminho da imagem no arquivo tsconfig.json

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,6 @@
 // Importando os componentes View e Text do react-native
-import {View, Text} from "react-native"
+import {View, Text, Image} from "react-native"
+import {styles} from "./styles"
 
 
 
@@ -8,18 +9,21 @@ import {View, Text} from "react-native"
 // --------------------------------- Regras para criar um componente -------------------------------- //
 // Componente tem que começar com letra maiúscula
 // O componente pode retornar um unico elemento pai, que pode ter varios elementos filhos dentro dele.
-export default function App(){ 
+export function Home(){ 
 
   // Toda função precisa retornar algo, o conteudo retornado aqui será exibido na tela.
   // View foi importado lá em cima, junto com o Text. "Essa View é o elemento pai" os elementos tem que estar dentro dela.
 
   return(
 
-    <View style={{ flex:1, justifyContent:"center"}}>
-        <Text>Olá Mundo!</Text> 
+    <View style={styles.container}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo}/>
+        
 
     </View>
     
     )
 
 }
+
+
