a7012370183f18ad99612b2c4a56d26c3b4af000
Matheus Melo <162399194+matfels@users.noreply.github.com>
2025-12-09 16:29:05 -0300

    Aula de ontem e hoje

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react"
 // Importando os componentes View e Text do react-native
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native"
+import { View, Image, TouchableOpacity, Text, FlatList, Alert } from "react-native"
 import { styles } from "./styles"
 import { Button } from "@/components/Button/index"
 import { Input } from "@/components/Input/index"
@@ -9,11 +10,7 @@ import { Item } from "@/components/Item"
 
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-const ITEMS = [
-  {id: "1", status: FilterStatus.DONE, description: "1 pacote de cafe"},
-  {id: "2", status: FilterStatus.PENDING, description: "3 pacote de MACARRÃO"},
-  {id: "3", status: FilterStatus.PENDING, description: "3 Cebolas"}  
-]
+
 
 // Exportando por padrão uma função vazia chamada App
 
@@ -21,42 +18,61 @@ const ITEMS = [
 // Componente tem que começar com letra maiúscula
 // O componente pode retornar um unico elemento pai, que pode ter varios elementos filhos dentro dele.
 export function Home() {
-  console.log("ITEMS", ITEMS)
+  //                      ESTADOOSSS
+  const [filter, setFilter] = useState(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
+  const [items, setItems] = useState<any>([])
+  function handleAdd() {
+    if (!description.trim()){
+      console.log("Chamando a função handleAdd, caixa de texto valia")
+      return Alert.alert("Adicionar","Informe a descrição para adicionar um item.")
+    }
+    const newItem = {
+      description,
+      id: Math.random().toString(36).substring(2),
+      status: FilterStatus.PENDING, 
+    }
+    
+    console.log(items)
+  }
+
   // Toda função precisa retornar algo, o conteudo retornado aqui será exibido na tela.
   // View foi importado lá em cima, junto com o Text. "Essa View é o elemento pai" os elementos tem que estar dentro dela.
-
   return (
-
     <View style={styles.container}>
-
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
-
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" onPress={() => console.log("Entrar")} />
+        <Input placeholder="O que você precisa comprar?"
+          onChangeText={setDescription}
+        />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
-
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
           <TouchableOpacity style={styles.clearButton}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
         <FlatList
-          data={[]}
+          data={items}
           keyExtractor={item => item.id}
-          renderItem={({item}) =>(
+          renderItem={({ item }) => (
             <Item
               data={item}
               onStatus={() => console.log("Muda o status")}
               onRemove={() => console.log("remover")}
-            />            
+            />
           )}
           showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={() => <View style={styles.separator}/>}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
           contentContainerStyle={styles.listContent}
           ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
         />
